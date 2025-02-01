8dc9a488355b78b034a353d71c760cfce907a630
show151 <136795114+show151@users.noreply.github.com>
2025-02-01 22:46:38 +0900

    14th commit

--- src/subject_selection.py
+++ src/subject_selection.py
@@ -1,9 +1,7 @@
 import PySide6.QtWidgets as Qw
 import PySide6.QtCore as Qc
 import english_word_dictionary as ewd
-import math_practice as mp
-import science_practice as sp
-
+import english_practice as ep
 sp_exp = Qw.QSizePolicy.Policy.Expanding
 
 class SubjectSelection(Qw.QWidget):
@@ -13,6 +11,7 @@ class SubjectSelection(Qw.QWidget):
     self.setWindowTitle("Subject Selection")
     self.setGeometry(150, 150, 300, 200)
 
+    self.word_dict = []  # Initialize word_dict as an empty list
     layout.setSpacing(20)
     layout.setAlignment(Qc.Qt.AlignmentFlag.AlignCenter)
 
@@ -31,23 +30,17 @@ class SubjectSelection(Qw.QWidget):
             }
         """
 
-    self.btn_math = Qw.QPushButton("Math")
-    self.btn_math.setStyleSheet(button_style)
-    self.btn_math.setSizePolicy(sp_exp, sp_exp)
-    self.btn_math.clicked.connect(self.open_math_practice)
-    layout.addWidget(self.btn_math)
-
-    self.btn_sci = Qw.QPushButton("Science")
-    self.btn_sci.setStyleSheet(button_style)
-    self.btn_sci.setSizePolicy(sp_exp, sp_exp)
-    self.btn_sci.clicked.connect(self.open_science_practice)
-    layout.addWidget(self.btn_sci)
-
-    self.btn_eng = Qw.QPushButton("English")
+    self.btn_eng = Qw.QPushButton("Dictionary")
     self.btn_eng.setStyleSheet(button_style)
     self.btn_eng.setSizePolicy(sp_exp, sp_exp)
     self.btn_eng.clicked.connect(self.open_english_word_dictionary)
     layout.addWidget(self.btn_eng)
+
+    self.btn_practice = Qw.QPushButton("Practice")
+    self.btn_practice.setStyleSheet(button_style)
+    self.btn_practice.setSizePolicy(sp_exp, sp_exp)
+    self.btn_practice.clicked.connect(self.open_english_practice)
+    layout.addWidget(self.btn_practice)
 
     self.btn_back = Qw.QPushButton("Back")
     self.btn_back.setStyleSheet(button_style)
@@ -55,17 +48,17 @@ class SubjectSelection(Qw.QWidget):
     self.btn_back.clicked.connect(self.close)
     layout.addWidget(self.btn_back)
 
-  def open_math_practice(self):
-    self.math_practice = mp.MathPractice()
-    self.math_practice.show()
-    self.close()
-
-  def open_science_practice(self):
-    self.science_practice = sp.SciencePractice()
-    self.science_practice.show()
-    self.close()
-
   def open_english_word_dictionary(self):
     self.english_practice = ewd.EnglishWordDictionary()
     self.english_practice.show()
-    self.close()
+    self.hide()
+
+  def open_english_practice(self):
+    if not self.word_dict:
+      Qw.QMessageBox.warning(
+          self, "No Words", "Please add words to the list to start practicing.")
+      return
+
+    self.practice_window = ep.EnglishPractice(self.word_dict, self)
+    self.practice_window.show()
+    self.hide()
